7325ceee2e241cf6eccfab707dec2090ec356dab
Sahil bisht <sahilbisht@Sahils-MacBook-Air.local>
2024-03-31 13:54:41 +0530

    web hook

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,14 +4,14 @@ import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
 import Tag from "@/database/tag.model";
 import User from "@/database/user.model";
-import { revalidatePath } from "next/cache";
 
 export async function getQuestions(params) {
   try {
     connectToDatabase();
     const questions = await Question.find({})
       .populate({ path: "tags", model: Tag })
-      .populate({ path: "author", model: User });
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
     return { questions };
   } catch (error) {
     console.log("Error in fetching questions", error);
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,15 +2,55 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { revalidatePath } from "next/cache";
+import path from "path";
 
 export async function getUserById(params: any) {
   try {
     connectToDatabase();
     const { userId } = params;
     console.log(userId);
-    const user = await User.findOne({clerkId:userId})
+    const user = await User.findOne({ clerkId: userId });
     return user;
   } catch (error) {
     console.log(error);
   }
+}
+
+export async function createUser(userParams: any) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userParams);
+    return newUser;
+  } catch (error) {
+    console.log("error in creating user", error);
+  }
+}
+
+export async function updateUser(params: any) {
+  const { clerkId, updatedData, path } = params;
+  try {
+    connectToDatabase();
+    await User.findOneAndUpdate(
+      {
+        clerkId,
+      },
+      updatedData,
+      { new: true }
+    );
+    revalidatePath(path);
+  } catch (error) {}
+}
+
+export async function deleteUser(params: any) {
+  const { clerkId } = params;
+  try {
+    connectToDatabase();
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // delete user from database
+    // and questions,answers,comments etc
+  } catch (error) {}
 }
